4d3e4128d2cebad8d94c13368965c3ae19cbfa25
xss9149 <wi22b099@technikum-wien.at>
2023-06-28 19:05:08 +0200

    calculateSum method created

--- src/main/java/com/example/exam/controller/FactorialController.java
+++ src/main/java/com/example/exam/controller/FactorialController.java
@@ -1,21 +1,35 @@
 package com.example.exam.controller;
+
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 
 public class FactorialController {
+
+    int sum = 0;
+
     @RequestMapping("/api/factorial")
     public int calculateFactorialValue(@RequestParam int number) {
-            if (number == 0 || number == 1) {
-                return 1;
-            }
-
-            int factorial = 1;
-            for (int i = 2; i <= number; i++) {
-                factorial *= i;
-            }
-
-            return factorial;
+        if (number == 0 || number == 1) {
+            return 1;
         }
+
+        int factorial = 1;
+        for (int i = 2; i <= number; i++) {
+            factorial *= i;
+        }
+        sum += factorial;
+
+        return factorial;
     }
+
+    @RequestMapping("/api/factorial/total")
+    public int caculateSum() {
+        return sum;
+
+    }
+
+
+}
+
